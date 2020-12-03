0451ca14c1f67a90d20f2c16437eeaeb6d775b7b
Sergiy Naumov <1414sergiy@gmail.com>
2020-12-03 10:51:35 +0200

    Third commit

--- Django_HW5/wsgi.py
+++ Django_HW5/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HomeWork_5 project.
+WSGI config for Django_HW5 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeWork_5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_HW5.settings')
 
 application = get_wsgi_application()
