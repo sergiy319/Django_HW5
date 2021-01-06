a7f633856507693582f96c7c76eb46f9cc9f18a6
Sergiy Naumov <1414sergiy@gmail.com>
2021-01-06 16:06:08 +0200

    First commit. Installed celery and rabbitmq-server, Created a celery.py file, Created an application in celery.py, Created a simple task in the application, Added broker settings in settings.py

---
+++ Django_HW5/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_HW5.settings')
+
+app = Celery('Django_HW5')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+# Создаём простейшую функцию, чтобы проверить,
+# что селери работает правильно.
+# Оборачиваем её в декоратор.
+@app.task(bind=True)
+def task_test(self):
+    return 2 + 2
