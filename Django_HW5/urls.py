3e88968d44a4cd08d5aed484e0ecd0406415851e
Sergiy Naumov <1414sergiy@gmail.com>
2021-02-02 14:02:58 +0200

    Sixth commit. Fixed bug of conflict between git branches.

--- Django_HW5/urls.py
+++ Django_HW5/urls.py
@@ -22,6 +22,7 @@ from home.views import (CSVView, HomeView, JsonView, SendMailView,
 from home.views import home as home_view
 from home.views import update_student
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', home_view, name='students'),
